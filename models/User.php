ca22647f66828aeea1151a090832b6b0486fe9aa
10xjzheng <10xjzheng@gmail.com>
2016-03-24 15:18:20 +0800

    【edit】md5(password)

--- models/User.php
+++ models/User.php
@@ -136,7 +136,7 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
      */
     public function validatePassword($password)
     {
-        return $this->password === ($password);
+        return $this->password === (md5($password));
     }
 
     /**
