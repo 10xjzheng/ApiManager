8f8b8bb4463bd8628171bcc41c832470695a9dd0
10xjzheng <10xjzheng@gmail.com>
2016-03-24 15:27:22 +0800

    【edit】-----

--- views/api/index.php
+++ views/api/index.php
@@ -13,7 +13,7 @@ use yii\widgets\ListView;
             <span style="font-size:30px;" class="glyphicon glyphicon-grain" aria-hidden="true"></span> <span style="font-size:16px;">欢迎使用接口管理工具 </span><br>
             <pre class="info" style="margin:10px 34px 10px 34px">
     什么是接口文档管理工具？
-    这是一个针对项目接口的管理工具，包括请求参数，返回参数的，测试请求，返回json数据的格式化是一个在线API文档系统。
+    这是一个针对项目接口的管理工具，包括请求参数，返回参数的，测试请求，返回json格式化数据。
     其致力于快速解决团队内部接口文档的编写、维护、存档，和减少团队协作开发的沟通成本。
             </pre>
         </div>
